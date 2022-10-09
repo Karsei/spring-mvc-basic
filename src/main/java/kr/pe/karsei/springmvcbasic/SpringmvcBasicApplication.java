2355bd6cd5175e3f276fa5d684d30386dec7db53
Karsei <karseidev@gmail.com>
2022-10-09 22:16:28 +0900

    initial commit

---
+++ src/main/java/kr/pe/karsei/springmvcbasic/SpringmvcBasicApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.karsei.springmvcbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmvcBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/pe/karsei/springmvcbasic/SpringmvcBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.karsei.springmvcbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
